6c0f32d8f36da9099d040750783669e6f0b0545c
Tkaixiang <tkaixiang@gmail.com>
2021-12-15 16:07:40 +0800

    fix

--- Utils/findBestMatchItem.ts
+++ Utils/findBestMatchItem.ts
@@ -1,4 +1,4 @@
-import stringSimilarity from 'string-similarity'
+const stringSimilarity = require("string-similarity");
 
 const findBestMatchItem = (query: string): string => {
     const matches = stringSimilarity.findBestMatch(query, NodeCacheObj.get("itemList") as string[]);
