68a93a8179aa2342c3e539f3ed0cce079fe3c12f
Tkaixiang <tkaixiang@gmail.com>
2021-12-22 18:56:36 +0800

    logging

--- Utils/stockpilerUpdateStockpile.ts
+++ Utils/stockpilerUpdateStockpile.ts
@@ -29,6 +29,7 @@ const stockpilerUpdateStockpile = async (client: Client, body: any, response: ht
             await collections.stockpiles.updateOne({ name: body.name.replace(".", "").replace("$", "") }, { $set: { items: newStockpileItems, lastUpdated: new Date() } })
         }
         else {
+            console.log('New stockpile: ' + body.name + ' added.')
             let newItems: any = {}
             for (let i = 0; i < body.data.length; i++) {
                 newItems[body.data[i][0]] = parseInt(body.data[i][1])
