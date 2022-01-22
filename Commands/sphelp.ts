746533f49af8181895ce16ace1a6c1285dc976ab
Tkaixiang <tkaixiang@gmail.com>
2022-01-22 10:56:50 +0800

    fix sphelp

--- Commands/sphelp.ts
+++ Commands/sphelp.ts
@@ -6,12 +6,12 @@ const sphelp = async (interaction: CommandInteraction): Promise<boolean> => {
         ephemeral: true
     });
 
-    let msg = `**__Foxhole Stockpiler Discord Bot Version 0.1__** 
+    let msg = `**__Foxhole Stockpiler Discord Bot Version 0.2 22/01/2022__** 
 **Commands:**
 - \`/sphelp\` Displays this message.
 - \`/spstatus\` Displays the current stockpile status. 🙍‍♂️
-- \`/spsetamount <item> <amount> <stockpile>\` Sets the \`<amount>\` that an \`<item>\` has in **__crates__** inside the \`<stockpile>\`. (E.g \`/spsettarget Bmats 100\`) 🙍‍♂️
-- \`/sptarget set <item> <amount>\` Sets the target \`<amount>\` that an \`<item>\` should have in **__crates__**. (E.g \`/spsettarget Bmats 100\`) 👨‍⚖️
+- \`/spsetamount <item> <amount> <stockpile>\` Sets the \`<amount>\` that an \`<item>\` has in **__crates__** inside the \`<stockpile>\`. (E.g \`/spsetamount set Basic Materials Crate 100\`) 🙍‍♂️
+- \`/sptarget set <item> <minimum_amount> <maximum_amount> <production_location>\` Sets the target \`<minimum_amount>\` that an \`<item>\` should have in **__crates__**. (E.g \`/sptarget set Basic Materials Crate 100\`) 👨‍⚖️
 - \`/sptarget remove <item>\` Removes a target item off the stockpile. 👨‍⚖️
 - \`/spstockpile add <stockpile>\` Adds an EMPTY stockpile with the name <stockpile>
 - \`/spstockpile remove <stockpile>\` Removes the <stockpile> from the stockpile status and listings. 👨‍⚖️
@@ -20,12 +20,12 @@ const sphelp = async (interaction: CommandInteraction): Promise<boolean> => {
 - \`/splogichannel remove\` Removes a logi channel in which stockpile information would always be displayed and updated on. 👨‍⚖️
 - \`/sprole add <perms> <role>\` Add a role which will be allowed to have <perms>. <perms> can be either "Admin" or "User". 👨‍⚖️
 - \`/sprole remove <role>\` Removes a role from whatever permissions they have. 👨‍⚖️
-- \`/spsetorder <stockpile> <order>\` Sets the <order> of a <stockpile> in the logi channel message.
-- \`/spsettimeleft <stockpile> <time>\` Sets the time left for a reserve <stockpile> before it expires. NOTE: <time> is a UNIX TIMESTAMP
-- \`/spnotif add <role>\` Adds a <role> to the notification stockpile expiry warning mention list.
-- \`/spnotif remove <role>\` Removes a <role> to the notification stockpile expiry warning mention list.
-- \`/spprettyname add <stockpile> <pretty_name>\` Adds a <pretty_name> to the <stockpile>. Pretty names are alternative names used to display the stockpile name instead of the original name.
-- \`/spprettyname remove <stockpile>\` Removes a pretty name from the <stockpile>. Pretty names are alternative names used to display the stockpile name instead of the original name.
+- \`/spsetorder <stockpile> <order>\` Sets the <order> of a <stockpile> in the logi channel message. 👨‍⚖️
+- \`/spsettimeleft <stockpile> <time>\` Sets the time left for a reserve <stockpile> before it expires. NOTE: <time> is a UNIX TIMESTAMP 👨‍⚖️
+- \`/spnotif add <role>\` Adds a <role> to the notification stockpile expiry warning mention list. 👨‍⚖️
+- \`/spnotif remove <role>\` Removes a <role> to the notification stockpile expiry warning mention list. 👨‍⚖️
+- \`/spprettyname add <stockpile> <pretty_name>\` Adds a <pretty_name> to the <stockpile>. Pretty names are alternative names used to display the stockpile name instead of the original name. 👨‍⚖️
+- \`/spprettyname remove <stockpile>\` Removes a pretty name from the <stockpile>. Pretty names are alternative names used to display the stockpile name instead of the original name. 👨‍⚖️
 
 
 👨‍⚖️ - Commands that require at least \`Admin\` permissions
