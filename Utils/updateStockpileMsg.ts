c3b104efb5d4d6ed513fa5fd1d6060a51ba4d70c
Tkaixiang <tkaixiang@gmail.com>
2022-07-29 18:41:03 +0800

    fix

--- Utils/updateStockpileMsg.ts
+++ Utils/updateStockpileMsg.ts
@@ -259,7 +259,7 @@ const updateStockpileMsg = async (client: Client, guildID: string | null, msg: [
                         editedMsgs = true
                         console.log(eventName + "Refresh stockpile button not found, sending a new 1")
                         const newMsg =  await channelObj.send({ content: "----------\nRefresh the timer of **all stockpiles**", components: [msg[4]] })
-                        await collections.config.updateOne({}, { $set: { refreshAllMsg: newMsg.id } })
+                        await collections.config.updateOne({}, { $set: { refreshAllID: newMsg.id } })
 
                         let targetMsgIDs = []
                         let targetMsgFuncArray = []
